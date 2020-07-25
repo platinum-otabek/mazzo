c744e827dab2b39db0ea9428f5b84cf1fff860d5
platinum-otabek <mr.tursinboev@gmail.com>
2020-07-25 18:58:40 +0500

    ru-en

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('en/index');
 });
 
 /* GET portfolio-details page. */
 router.get('/portfolio-details', (req, res, next)=> {
-  res.render('portfolio_details');
+  res.render('en/portfolio_details');
 });
 
 module.exports = router;
