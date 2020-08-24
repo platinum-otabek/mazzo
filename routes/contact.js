3d90e6ee1d6beb6b694c215302dea9e417280f00
platinum-otabek <mr.tursinboev@gmail.com>
2020-08-24 11:25:38 +0500

    nginx

--- routes/contact.js
+++ routes/contact.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Telegraf = require('telegraf');
-const bot = new Telegraf("1379579193:AAGoH6lMwmVSBCJWL7TQz1vx8F9xNhXigCc");
-
-
-router.post('/',async (req,res,next)=>{
-    let message = `name: ${req.body.name} \n number: ${req.body.number} \n email: ${req.body.email} \n subject: ${req.body.subject} \n message: ${req.body.message}`;
-    await bot.telegram.sendMessage('716916580', message,{
-        parse_mode:'HTML'
-    });
-    bot.launch();
-    res.redirect(`/${req.body.direc}`);
-});
+// const Telegraf = require('telegraf');
+// const bot = new Telegraf("1379579193:AAGoH6lMwmVSBCJWL7TQz1vx8F9xNhXigCc");
+//
+//
+// router.post('/',async (req,res,next)=>{
+//     let message = `name: ${req.body.name} \n number: ${req.body.number} \n email: ${req.body.email} \n subject: ${req.body.subject} \n message: ${req.body.message}`;
+//     await bot.telegram.sendMessage('716916580', message,{
+//         parse_mode:'HTML'
+//     });
+//     bot.launch();
+//     res.redirect(`/${req.body.direc}`);
+// });
 
 
 
